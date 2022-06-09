c111ad2f5083eb1ca4624d6098c1f074ccd0b721
Duewiger <kd@kilianduewiger.com>
2022-06-10 00:46:13 +0200

    Test Github on "gohard"

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>    
+
+<?php 
+
+echo "<p>Copyright &copy; 2019-" . date("Y") . " gohard.com</p>";
+
+?>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include __DIR__ .'/head.php' ?>
+
+<body>
+
+
+<?php require 'footer.php' ?>   
+
+</body>
+</html>
